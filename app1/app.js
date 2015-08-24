1f9bd81fab3ae203713427b88d48949a75944570
ayrokid <ayrokid@gmail.com>
2015-08-24 07:45:09 +0700

    mysql

--- app1/app.js
+++ app1/app.js
@@ -8,11 +8,23 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var myConnection = require('express-myconnection'), // express-myconnection module
+    dbOptions = {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      port: 3306,
+      database: 'nodejs'
+    };
+
+
+var mysql = require('mysql');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +33,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * connection
+ */
+app.use(myConnection(mysql, dbOptions, 'request'));
 
 app.use('/', routes);
 app.use('/users', users);
--- app1/routes/users.js
+++ app1/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+
+/* MySQL connection */
+var mysql   = require('mysql');
+var pool    = mysql.createPool({
+  host : 'localhost',
+  user : 'root',
+  password : 'root',
+  database : 'nodejs'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.getConnection(function(err, conn){
+    conn.query('select * from customer', function(err, rows){
+      if(err)
+        console.log('Error Selecting : %s', err);
+
+      res.render('customers', {page_title: 'Customers - Node.js', data:rows});
+    });
+  });
+  //res.send('respond with a resource');
 });
 
 module.exports = router;
