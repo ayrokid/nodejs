d36459939af40a37086e0296e8076045b184d4fa
ayrokid <ayrokid@gmail.com>
2015-09-03 07:24:05 +0700

    modal

--- ionic/app3/www/js/app.js
+++ ionic/app3/www/js/app.js
@@ -17,3 +17,32 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+<!-- custome here -->
+var app = angular.module('starter2', ['ionic'])
+
+app.controller('MainCtrl', function($scope, $ionicModal) {
+  $scope.contact = {
+    name: 'Mittens Cat',
+    info: 'Tap anywhere on the card to open the modal'
+  }
+
+  $ionicModal.fromTemplateUrl('contact-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  })
+
+  $scope.openModal = function() {
+    $scope.modal.show()
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+})
