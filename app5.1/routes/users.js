605cd5a5174066fc8ba540d30f9ab12e843bb5dd
ayrokid <ayrokid@gmail.com>
2015-09-23 07:31:06 +0700

    app5.1 for scraping

---
+++ app5.1/routes/users.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var fs 		= require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var router 	= express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/scrape', function(req, res, next) {
+  url = 'http://www.imdb.com/title/tt1229340/';
+
+  request(url, function(error, response, html) {
+
+  	if(!error){
+  		var $ = cheerio.load(html);
+
+  		var title, release, rating;
+
+  		var json = {title : "", release : "", rating : ""};
+
+  		$('.header').filter(function(){
+  			var data = $(this);
+
+  			title = data.children().first().text();
+
+  			release = data.children().last().children().text();
+
+  			json.title = title;
+
+  			json.release= release;
+  		});
+
+  		$('.star-box-giga-star').filter(function(){
+            var data = $(this);
+
+            // The .star-box-giga-star class was exactly where we wanted it to be.
+            // To get the rating, we can simply just get the .text(), no need to traverse the DOM any further
+
+            rating = data.text();
+
+            json.rating = rating;
+        });
+
+  	}
+
+  	fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+	    console.log('File successfully written! - Check your project directory for the output.json file');
+
+	})
+
+	// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+	res.send('Check your console!')
+
+  });
+
+});
+
+module.exports = router;
