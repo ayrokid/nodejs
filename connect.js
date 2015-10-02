afa6b6fa9df347d038aaefb83d5d72493b091584
ayrokid <ayrokid@gmail.com>
2015-10-02 17:16:04 +0700

    mongodb

--- connect.js
+++ connect.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://127.0.0.1/27017/test');
+var db = mongoose.connect('mongodb://localhost/beardb');
+
+//var db = mongoose.connect('mongodb://192.168.3.167/27017/sso_comment');
 
 mongoose.connection.once('connected', function(){
 	console.log('Database connected successfully');
