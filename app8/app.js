9b86ec110d83ff3fa233e5abfb1eb1b0ef5c554d
ayrokid <ayrokid@gmail.com>
2015-10-29 13:29:30 +0700

    app8 for auth server

--- app8/app.js
+++ app8/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.oauth = oauthserver ({
   model: {},
