14c52e571614b9b9ab9c4f526140a6c12c1f93fa
ayrokid <ayrokid@gmail.com>
2016-08-25 18:01:59 +0700

    sync

---
+++ basic/modules/bucky.js
@@ -0,0 +1,3 @@
+var movies = require('./movies');
+
+console.log("bucky's favorite movie is : " + movies.favMovie);
---
+++ basic/modules/emily.js
@@ -0,0 +1,5 @@
+var movies = require('./movies');
+
+movies.favMovie = "the motebook";
+
+console.log("emily's favorite movie is : " + movies.favMovie);
